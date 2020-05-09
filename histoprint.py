4e63b91fda6e8e311b605812f9f75763d3a16b38
Lukas Koch <lukas.koch@mailbox.org>
2020-05-09 21:08:14 +0100

    Add proper overlay functionality.

--- histoprint.py
+++ histoprint.py
@@ -7,6 +7,108 @@ import sys
 import numpy as np
 
 
+class Hixel(object):
+    """The smallest unit of a histogram plot."""
+
+    def __init__(self, char=" ", fg="0", bg="0", use_color=True):
+        self.character = char
+        self.fg_color = fg
+        self.bg_color = bg
+        self.use_color = use_color
+
+    def add(self, char=" ", fg="0", bg="0"):
+        """Add another element on top."""
+
+        if char != " " and fg == self.fg_color:
+            # Combine characters if possible
+            self.character = self.overlay_characters(self.character, char)
+        else:
+            self.character = char
+            self.fg_color = fg
+
+        if bg != "0":
+            self.bg_color = bg
+
+    def render(self, reset=True):
+        """Render the Hixel as a string."""
+        ret = ""
+        if self.character == " " and self.bg_color != "0":
+            # Instead of printing a space with BG color,
+            # print a full block with same FG color,
+            # so the histogram can be copied to text editors.
+            ret += self.ansi_color_string(self.bg_color, self.bg_color)
+            ret += u"\u2588"
+        else:
+            ret += self.ansi_color_string(self.fg_color, self.bg_color)
+            ret += self.substitute_character(self.character)
+        if reset:
+            ret += self.ansi_color_string("0", "0")
+        return ret
+
+    @staticmethod
+    def substitute_character(char):
+        r"""Replace some ASCII characters with better looking Unicode.
+
+        Substitutions::
+
+            "-" -> "\u2500"
+            "|" -> "\u2502"
+            "=" -> "\u2550"
+            "+" -> "\u253C"
+            "#" -> "\u256A"
+
+        """
+
+        subs = {
+            "-": u"\u2500",
+            "|": u"\u2502",
+            "=": u"\u2550",
+            "+": u"\u253C",
+            "#": u"\u256A",
+        }
+
+        return subs.get(char, char)
+
+    @staticmethod
+    def overlay_characters(char1, char2):
+        r"""Combine character to give the impression of drawing them both.
+
+        Possible combinations::
+
+            "-" + "|" -> "+"
+            "=" + "|" -> "#"
+            "/" + "\" -> "X"
+
+        """
+
+        subs = {
+            ("-", "|"): "+",
+            ("=", "|"): "#",
+            ("/", "\\"): "X",
+        }
+        return subs.get((char1, char2), subs.get((char2, char1), char2))
+
+    def ansi_color_string(self, fg, bg):
+        """Set the terminal color."""
+        ret = ""
+        if self.use_color:
+            fg = int(fg, 16) + 30
+            bg = int(bg, 16) + 40
+            if fg > 37:
+                fg += 52
+            if bg > 47:
+                bg += 52
+            if fg == 30:
+                ret += "\033[%dm" % (39,)
+            else:
+                ret += "\033[%dm" % (fg,)
+            if bg == 40:
+                ret += "\033[%dm" % (49,)
+            else:
+                ret += "\033[%dm" % (bg,)
+        return ret
+
+
 class BinFormatter(object):
     """Class that turns bin contents into text.
 
@@ -28,6 +130,18 @@ class BinFormatter(object):
         Character to be printed for axis without tick.
     print_top_edge : bool
         Whether to print the top or bottom edge of the bin.
+    symbols : iterable
+        The foreground symbols to be used for the histograms.
+    colors : iterable
+        The foreground colors (single digit hex value) to be used.
+        A value of 0 means the default terminal color.
+    bg_colors : iterable
+        The background colors (single digit hex value) to be used.
+        A value of 0 means "transparent".
+    stack : bool
+        Whether to stack the histograms, or overlay them.
+    use_color : bool
+        Whether to use color output.
 
     """
 
@@ -40,7 +154,11 @@ class BinFormatter(object):
         tick_mark="_",
         no_tick_mark=" ",
         print_top_edge=False,
-        symbols="#XO",
+        symbols=" |=",
+        colors="000",
+        bg_colors="300",
+        stack=False,
+        use_color=True,
     ):
         self.scale = scale
         self.count_area = count_area
@@ -50,6 +168,10 @@ class BinFormatter(object):
         self.no_tick_mark = no_tick_mark
         self.print_top_edge = print_top_edge
         self.symbols = symbols
+        self.colors = colors
+        self.bg_colors = bg_colors
+        self.stack = stack
+        self.use_color = use_color
 
     def format_bin(self, top, bottom, counts, width=1):
         """Return a string that represents the bin.
@@ -89,8 +211,27 @@ class BinFormatter(object):
                 bin_string += self.no_tick()
 
             # Print symbols
-            for h, s in zip(heights, self.symbols):
-                bin_string += s * h
+            line = []
+            for h, s, c, bg in zip(heights, self.symbols, self.colors, self.bg_colors):
+                if h:
+                    if self.stack:
+                        # Just print them all afer one another
+                        line += [Hixel(s, c, bg, self.use_color) for _ in range(h)]
+                    else:
+                        # Overlay histograms
+                        if h > len(line):
+                            for hix in line:
+                                hix.add(s, c, bg)
+                            line += [
+                                Hixel(s, c, bg, self.use_color)
+                                for _ in range(h - len(line))
+                            ]
+                        else:
+                            for hix in line[:h]:
+                                hix.add(s, c, bg)
+
+            for hix in line:
+                bin_string += hix.render()
 
             # New line
             bin_string += "\n"
@@ -118,6 +259,10 @@ class HistFormatter(object):
         Whether the bin content is represented by the area or height of the bin.
     scale_bin_width : bool
         Whether the lines per bin should scale with the bin width.
+    title : str
+        Title string to print over the histogram.
+    stack : bool
+        Whether to stack or overlay the histograms.
 
     """
 
@@ -129,6 +274,7 @@ class HistFormatter(object):
         count_area=True,
         scale_bin_width=True,
         title="",
+        stack=False,
     ):
         self.title = title
         self.edges = edges
@@ -146,7 +292,7 @@ class HistFormatter(object):
             line_scale = np.max(edges[1:] - edges[:-1])
         self.bin_lines = ((edges[1:] - edges[:-1]) // line_scale).astype(int)
         self.bin_lines = np.where(self.bin_lines, self.bin_lines, 1)
-        self.bin_formatter = BinFormatter()
+        self.bin_formatter = BinFormatter(stack=stack, count_area=count_area)
 
     def format_histogram(self, counts):
         """Format (a set of) histogram counts."""
@@ -157,28 +303,48 @@ class HistFormatter(object):
         hist_width = self.columns - axis_width
 
         counts = np.array(counts)
-        c = np.sum(counts, axis=0) / self.bin_lines
+        while counts.ndim < 2:
+            # Make sure counts is a 2D array
+            counts = counts[np.newaxis, ...]
+
+        if self.bin_formatter.stack:
+            # Bin content will be sum of all histograms
+            c = np.sum(counts, axis=0)
+        else:
+            # Bin content will be maximum of all histograms
+            c = np.max(counts, axis=0)
+        if self.bin_formatter.count_area:
+            # Bin content will be divided by number of lines
+            c = c / self.bin_lines
+
+        # Set a scale so that largest bin wills width of allocated area
         symbol_scale = np.max(c) / hist_width
         self.bin_formatter.scale = symbol_scale
 
+        # Write the title line
         hist_string = ("{:^%ds}\n" % (self.columns,)).format(self.title)
+
         top = self.edges[:-1]
         bottom = self.edges[1:]
 
+        # Write the first tick
         hist_string += self.bin_formatter.tick(top[0]) + "\n"
 
+        # Write the bins
         for c, t, b, w in zip(counts.T, top, bottom, self.bin_lines):
             hist_string += self.bin_formatter.format_bin(t, b, c, w)
 
         return hist_string
 
 
-def print_hist(hist, file=sys.stdout, title=""):
+def print_hist(hist, file=sys.stdout, title="", stack=False, count_area=True):
     """plot the output of ``numpy.histogram`` to the console."""
 
     count, edges = hist
-    hist_formatter = HistFormatter(edges, title=title)
-    print_(hist_formatter.format_histogram([count]), end="", file=file)
+    hist_formatter = HistFormatter(
+        edges, title=title, stack=stack, count_area=count_area
+    )
+    print_(hist_formatter.format_histogram(count), end="", file=file)
 
 
 def text_hist(*args, **kwargs):
@@ -186,29 +352,34 @@ def text_hist(*args, **kwargs):
 
     file = kwargs.pop("file", sys.stdout)
     title = kwargs.pop("title", "")
-    hist = np.histogram(*args, **kwargs)
-    print_hist(hist, file=file, title=title)
+    stack = kwargs.pop("stack", False)
+    count_area = kwargs.pop("count_area", True)
+    density = kwargs.pop("density", False)
+    if density:
+        count_area = False
+    hist = np.histogram(*args, density=density, **kwargs)
+    print_hist(hist, file=file, title=title, stack=stack, count_area=count_area)
     return hist
 
 
 def test_hist():
     """Poor man's unit tests."""
 
-    A = np.random.randn(1000)
-    B = np.random.randn(1000) + 2
+    A = np.random.randn(1000) - 2
+    B = np.random.randn(1000)
+    C = np.random.randn(1000) + 2
 
-    print_("")
-    text_hist(A, bins=10)
-    print_("")
-    text_hist(A, bins=15)
-    print_("")
-    text_hist(A, bins=20)
-    print_("")
-    text_hist(A, bins=21)
-    print_("")
-    text_hist(A, bins=[-3, -2, -1, -0.5, 0, 0.5, 1, 2, 3])
-    print_("")
-    text_hist(A, bins=[-5, -3, -2, -1, -0.5, 0, 0.5, 1, 2, 3, 5], title="Test")
+    text_hist(A, bins=10, title="10 bins")
+    text_hist(A, bins=20, title="20 bins")
+    text_hist(A, bins=[-5, -3, -2, -1, -0.5, 0, 0.5, 1, 2, 3, 5], title="Variable bins")
+
+    histA = np.histogram(A, bins=20, range=(-5, 5))
+    histB = np.histogram(B, bins=20, range=(-5, 5))
+    histC = np.histogram(C, bins=20, range=(-5, 5))
+    histAll = ([histA[0], histB[0], histC[0]], histA[1])
+
+    print_hist(histAll, title="Overlays")
+    print_hist(histAll, title="Stack", stack=True)
 
 
 if __name__ == "__main__":
