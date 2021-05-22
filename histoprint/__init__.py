632d3f1f6c94d1b0db9bf0c368d4afb68aaacb57
Lukas Koch <lukas.koch@mailbox.org>
2021-05-22 18:20:25 +0100

    Do not import * in __init__.py

--- histoprint/__init__.py
+++ histoprint/__init__.py
@@ -1,5 +1,7 @@
 """histoprint - Pretty-print histograms to the terminal"""
 
-from histoprint.formatter import *
+from histoprint.formatter import HistFormatter, print_hist, text_hist
 
-from .version import version as __version__
+from .version import version as __version__  # noqa: F401
+
+__all__ = ["print_hist", "text_hist", "formatter", "HistFormatter"]
