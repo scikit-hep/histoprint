7f63eb0e38c93f39446e4a59bf45c0a571d933b3
Lukas Koch <lukas.koch@mailbox.org>
2021-05-25 15:02:15 +0100

    Use tuple instead of list for __all__.

--- histoprint/__init__.py
+++ histoprint/__init__.py
@@ -4,4 +4,4 @@ from histoprint.formatter import HistFormatter, print_hist, text_hist
 
 from .version import version as __version__  # noqa: F401
 
-__all__ = ["print_hist", "text_hist", "formatter", "HistFormatter"]
+__all__ = ("print_hist", "text_hist", "formatter", "HistFormatter")
